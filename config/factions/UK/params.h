8248ad20d5125214741512f540ed274fa561a04a
Alessandro Romanelli <aleromrod@gmail.com>
2018-10-08 12:01:14 +0200

    Initial commit for UK faction

---
+++ config/factions/UK/params.h
@@ -0,0 +1,2 @@
+values[] = {0,1,2,3,4,5,6,7};
+texts[] = {"M-1941 (Army)", "M-1941 HBT (Rangers)", "M-1941 (NAC)", "M42 (Airborne)", "M43 (Airborne)", "M-1941 (Army Winter)", "M42 (Airborne Winter)", "M43 (Airborne Winter)"};
---
+++ config/factions/UK/unlocks.h
@@ -0,0 +1,118 @@
+faction = "US";
+
+class LIB_M1_Garand {
+  ammo = "LIB_8Rnd_762x63";
+  exp = 0;
+  type = "primary";
+  roles[] = {"assault", "medic"};
+  damageMultiplier = 0.25;
+  class attachments {};
+};
+
+class LIB_Colt_M1911 {
+  ammo = "LIB_7Rnd_45ACP";
+  exp = 5000;
+  type = "secondary";
+  roles[] = {"assault", "medic", "engineer", "support"};
+  damageMultiplier = 0.5;
+  class attachments {};
+};
+
+class LIB_DELISLE {
+  ammo = "LIB_7Rnd_45ACP";
+  exp = 7500;
+  type = "primary";
+  roles[] = {"assault", "medic"};
+  damageMultiplier = 1;
+  class attachments {};
+};
+
+class LIB_M1_Carbine {
+  ammo = "LIB_15Rnd_762x33";
+  exp = 12500;
+  type = "primary";
+  roles[] = {"medic"};
+  damageMultiplier = 0.2;
+  class attachments {};
+};
+
+class LIB_M1A1_Carbine {
+  ammo = "LIB_15Rnd_762x33";
+  exp = 12500;
+  type = "primary";
+  roles[] = {"assault"};
+  damageMultiplier = 0.2;
+  class attachments {};
+};
+
+class LIB_M1918A2_BAR {
+  ammo = "LIB_20Rnd_762x63";
+  exp = 0;
+  type = "primary";
+  roles[] = {"support"};
+  damageMultiplier = 0.1;
+  class attachments {};
+};
+
+class LIB_M1919A4 {
+  ammo = "LIB_50Rnd_762x63";
+  exp = 7500;
+  type = "primary";
+  roles[] = {"support"};
+  damageMultiplier = 0.04;
+  class attachments {};
+};
+
+class LIB_M1919A6 {
+  ammo = "LIB_50Rnd_762x63";
+  exp = 12500;
+  type = "primary";
+  roles[] = {"support"};
+  damageMultiplier = 0.06;
+  class attachments {};
+};
+
+class LIB_M3_GreaseGun {
+  ammo = "LIB_30Rnd_M3_GreaseGun_45ACP";
+  exp = 0;
+  type = "primary";
+  roles[] = {"engineer"};
+  damageMultiplier = 0.2;
+  class attachments {};
+};
+
+class LIB_M1A1_Thompson {
+  ammo = "LIB_30Rnd_45ACP";
+  exp = 7500;
+  type = "primary";
+  roles[] = {"engineer"};
+  damageMultiplier = 0.3;
+  class attachments {};
+};
+
+class LIB_M1928_Thompson {
+  ammo = "LIB_50Rnd_45ACP";
+  exp = 12500;
+  type = "primary";
+  roles[] = {"engineer"};
+  damageMultiplier = 0.3;
+  class attachments {};
+};
+
+class LIB_M1903A3_Springfield {
+  ammo = "LIB_5Rnd_762x63";
+  exp = 0;
+  type = "primary";
+  roles[] = {"recon"};
+  damageMultiplier = 1;
+  class attachments {};
+};
+
+class LIB_M1903A4_Springfield {
+  ammo = "LIB_5Rnd_762x63";
+  exp = 7500;
+  type = "primary";
+  roles[] = {"recon"};
+  damageMultiplier = 1;
+  class attachments {};
+};
