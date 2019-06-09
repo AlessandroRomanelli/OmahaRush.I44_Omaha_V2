65c3782f4fb964959933cd735e372236a5d83fd1
Alessandro Romanelli <aleromrod@gmail.com>
2019-06-09 15:58:31 +0200

    Fixed restrictions

--- bin/restrictions.h
+++ bin/restrictions.h
@@ -2,9 +2,9 @@
 // 1 = ALL CLIENTS, NO SERVER
 // 2 = ONLY SERVER
 
+class client_fnc_meleeTakedown 				{allowedTargets = 0;};
 class client_fnc_MCOMarmed 					{allowedTargets = 0;};
 class client_fnc_displayError 				{allowedTargets = 1;};
-class client_fnc_meleeTakedown 				{allowedTargets = 1;};
 class client_fnc_displayObjectiveMessage 	{allowedTargets = 1;};
 class client_fnc_restoreAmmo 				{allowedTargets = 1;};
 class client_fnc_spawnBeaconSoundLoop 		{allowedTargets = 1;};
